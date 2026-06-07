407b25bccfc1356f8e56b3ec1b585e4de2394408
shivainnhotel <shivainnhotel224@gmail.com>
2026-06-07 20:16:22 +0530

    robots updated

--- app/robots.ts
+++ app/robots.ts
@@ -1,32 +1,13 @@
 import type { MetadataRoute } from "next";
 
-const baseUrl = "https://shivainn.com";
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/dining`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/bar-lounge`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/banquets`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-  ];
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://shivainn.com/sitemap.xml",
+  };
 }
