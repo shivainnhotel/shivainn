ef2e53d4c06e5094edbda1df25a92b567193f835
shivainnhotel <shivainnhotel224@gmail.com>
2026-06-09 14:13:12 +0530

    changes in the fevicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,7 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://shivainn.com"),
 
   icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
+    icon: "/favicon.ico",
     shortcut: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
